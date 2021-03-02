06d49cb6031e92e79926655f7a0277a462803586
Michael Matich <matich.mike@gmail.com>
2021-03-02 14:19:58 -0500

    tuple example

--- understandingTS/app.ts
+++ understandingTS/app.ts
@@ -1,7 +1,14 @@
 const person: {
   name: string;
-  age: number
+  age: number;
+  hobbies: string[];
+  role: [number, string]
 } = {
   name: "Mike",
-  age: 31
+  age: 31,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 }
+
+let favActivities: string[];
+let anyArr: any[];
