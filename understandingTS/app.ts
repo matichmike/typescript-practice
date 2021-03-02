c1f0a94d2ebdde2fb134e3d3649447b3d812a1a0
Michael Matich <matich.mike@gmail.com>
2021-03-02 14:47:06 -0500

    union types

--- understandingTS/app.ts
+++ understandingTS/app.ts
@@ -1,14 +1,21 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string, 
+  input2: number | string, 
+  resultConversion: 'as-num' | 'as-text'
+  ) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-num') {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  
-  return result;
+  if (resultConversion === 'as-num') {
+    return +result;
+  } else {
+    return result.toString();
+  }
 }
 
-const combineNums = combine(30, 25);
+const combineNums = combine(30, 25, 'as-num');
 
-const combineNames = combine("David", "Smith");
+const combineNames = combine("David", "Smith", 'as-text');
