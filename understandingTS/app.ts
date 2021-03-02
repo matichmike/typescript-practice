40ef3e35804d0adb6f7705161c21479248d323fd
Michael Matich <matich.mike@gmail.com>
2021-03-02 14:27:47 -0500

    enum example

--- understandingTS/app.ts
+++ understandingTS/app.ts
@@ -1,13 +1,19 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]
-} = {
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
+
+const person
+// : {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+//   auth: enum
+// }
+ = {
   name: "Mike",
   age: 31,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: [2, 'author'],
+  auth: Role.ADMIN
 }
 
 let favActivities: string[];
