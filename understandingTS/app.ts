4018bbf49d3366a33f493b3db1d0f54e94aa10ed
Michael Matich <matich.mike@gmail.com>
2021-03-02 15:24:25 -0500

    unknown type

--- understandingTS/app.ts
+++ understandingTS/app.ts
@@ -1,19 +1,8 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+userInput = 5;
+userInput = "John";
+
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-function printResult(num: number) {
-  console.log('Result: ' + num);
-}
-
-function addHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combineVals: (a: number, b: number) => number;
-combineVals = add;
-
-addHandle(1,2, (result) => {console.log(result)})
