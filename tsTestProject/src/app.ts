49a31d4372198395f3ffcc6b50b0df4d8f8932fb
Michael Matich <matich.mike@gmail.com>
2021-03-10 21:30:51 -0500

    ts classes

--- tsTestProject/src/app.ts
+++ tsTestProject/src/app.ts
@@ -1,20 +1,33 @@
-// Code goes here!
-const printOutput = (output: string | number) => console.log(output);
+// // Code goes here!
+// const printOutput = (output: string | number) => console.log(output);
 
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Hiking'];
-activeHobbies.push(...hobbies);
+// const hobbies = ['Sports', 'Cooking'];
+// const activeHobbies = ['Hiking'];
+// activeHobbies.push(...hobbies);
 
-const person = {
-  name: "Mike",
-  age: 15
+// const person = {
+//   firstName: "Mike",
+//   age: 15
+// }
+
+// const copiedPerson = {...person};
+
+// const add = (...numbers: number[]) => {
+//   return numbers.reduce((currResult, currValue) => {
+//     return currResult + currValue;
+//   }, 0)
+// }
+// const addedNumbers = add(5, 10, 2, 3);
+
+// const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+// const {firstName: userName, age} = person;
+
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
 }
 
-const copiedPerson = {...person};
-
-const add = (...numbers: number[]) => {
-  return numbers.reduce((currResult, currValue) => {
-    return currResult + currValue;
-  }, 0)
-}
-const addedNumbers = add(5, 10, 2, 3)
+new Department("Argument");
